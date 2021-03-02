9cb89a234a7b7a15c09c7a76fe0977478b9faeae
MarcZX10 <67475268+MarcZX100@users.noreply.github.com>
2021-03-02 09:55:23 +0100

    Update horoscope.js

--- commands/fun/horoscope.js
+++ commands/fun/horoscope.js
@@ -8,17 +8,24 @@ module.exports = {
 	enabled: true,
   exec: async (client, message, args) => {
 
-let luck = Math.floor((Math.random() * 100) + 1);
-let health = Math.floor((Math.random() * 100) + 1);
-let money = Math.floor((Math.random() * 100) + 1);
-let love = Math.floor((Math.random() * 100) + 1);
-
 if(message.author.id == "689106697561702437") {
 	let luck = "100"
 	let health = "100"
 	let money = "100"
         let love = "100"
-	}		
-message.channel.send(`> ${message.author} here's your horoscope: \n*Luck:* **${luck}**| *Health:* **${health}**| *Love:* **${love}**| *Money:* **${money}**`)
+        message.channel.send(`> ${message.author} here's your horoscope: \n*Luck:* **${luck}**| *Health:* **${health}**| *Love:* **${love}**| *Money:* **${money}**`)
+
+	};
+if(message.author.id !== "689106697561702437") {
+	
+ let luck = Math.floor((Math.random() * 100) + 1);
+ let health = Math.floor((Math.random() * 100) + 1);
+ let money = Math.floor((Math.random() * 100) + 1);
+ let love = Math.floor((Math.random() * 100) + 1);
+  message.channel.send(`> ${message.author} here's your horoscope: \n*Luck:* **${luck}**| *Health:* **${health}**| *Love:* **${love}**| *Money:* **${money}**`)
+
+}
+
+
 
 }}

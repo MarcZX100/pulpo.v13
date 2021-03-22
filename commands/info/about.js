6230097fe5a5aa6215af7e685ec2198d0df499d5
MarcZX10 <67475268+MarcZX100@users.noreply.github.com>
2021-03-22 09:50:47 +0100

    Update about.js

--- commands/info/about.js
+++ commands/info/about.js
@@ -12,12 +12,6 @@ module.exports = {
   permissions: [],
   enabled: true,
 	exec: async (client, message, args) => {
-		
-
-		
-var emcolor = [0]
-    if (message.guild) emcolor = message.guild.me.displayHexColor
-    if (!message.guild) emcolor = client.config.colors.general
 var lang = []
         if (message.guild) lang = message.guild.language 
         if (!message.guild) lang = message.channel.language
@@ -25,7 +19,7 @@ var lang = []
 
     const about = new Discord.MessageEmbed()
     .setTitle(client.user.tag)
-    .setColor(emcolor)
+    .setColor(client.color)
     .addField("Acknowledgements", `${client.users.cache.get(client.config.members.nath).tag} ${client.users.cache.get(client.config.members.candela).tag} ${client.users.cache.get(client.config.members.pulpo).tag} ${client.users.cache.get(client.config.members.piedra).tag}`)
     message.channel.send(about)
        

9553369472ab14b54c1d8f6eb57c090352da8941
MarcZX10 <67475268+MarcZX100@users.noreply.github.com>
2021-03-22 09:52:48 +0100

    Update invite.js

--- commands/info/invite.js
+++ commands/info/invite.js
@@ -12,16 +12,10 @@ module.exports = {
 	guildOnly: false,
 	enabled: true,
   exec: async (client, message, args) => {
-    
-
-    var emcolor = [0]
-    if (message.guild) emcolor = message.guild.me.displayHexColor
-      
-    if (!message.guild) emcolor = client.config.colors.general
       
 const embed = new Discord.MessageEmbed()
 .setTitle("Invite Links")
-.setColor(emcolor)
+.setColor(client.color)
 .addField("Official discord server",`[Server Link](https://discord.gg/nzwDdEM)`)
 .addField("Invite me to your server",`[Bot Link](https://discord.com/oauth2/authorize?client_id=758388900468883466&permissions=8&scope=bot)`)
 .setTimestamp(new Date())
